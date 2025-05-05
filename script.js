647c89686bba7a1f51e8f6c599f8650e35498444
Bo1129 <bohmb.22d@acsjszki.hu>
2025-05-05 11:15:05 +0200

    Kártyák sokszorosítása

--- script.js
+++ script.js
@@ -7,5 +7,28 @@ fetch('https://api.sampleapis.com/countries/countries')
         })
         
         console.log(countries);
+        loadCards(countries);
     })
     .catch(error => console.log('Hiba: ', error));
+
+function loadCards(countries){
+    const cardContainer = document.getElementById('card-container');
+
+    if(!countries || Object.keys(countries).length === 0){
+        console.error('Hiba: nincsenek országok');
+        return;
+    }
+
+    for(const key in countries){
+       const card = document.createElement('div');
+       card.classList.add('card');
+       card.innerHTML = `
+       <h2>${countries[key].name}</h2>
+       <img src="${countries[key].media.flag}" alt="${countries[key].name}_zaszlo">
+       <p><strong>Főváros: </strong> ${countries[key].capital}</p>
+       <p><strong>Pénznem: </strong> ${countries[key].currency}</p>
+       <p><strong>Rövidítés: </strong> ${countries[key].abbreviation}</p>`;
+
+       cardContainer.appendChild(card);
+    }
+}
